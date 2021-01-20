b1157de63bf5a28659dcae4fafe4334356cc14c0
jesso1908 <65100405+jesso1908@users.noreply.github.com>
2021-01-20 09:26:54 -0500

    Add files via upload

---
+++ intro_1.py
@@ -0,0 +1,11 @@
+# name=input("Enter the name:")
+# print(name)
+a=b=c=100
+print(a,b,c)
+d,e,f=200,100,150
+print(d,e,f)
+username,age="jesso",23
+print(username,age)
+volume=50000
+turnover=34000
+print(volume+turnover)
