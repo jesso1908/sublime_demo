dffe10a25f373c07d6022c70e4159cb846143316
jesso john joy <jesso1908joy@gmail.com>
2021-01-20 13:53:10 -0500

    first_1

---
+++ intro.py
@@ -0,0 +1,21 @@
+print("jesso")
+print("jesso","dean")
+print(15)
+print(45.7)
+print(45,56.54)
+print("Nifty:",14433.70,"Banknifty",32314.50)
+print(" $\n","$ $\n","$ $ $\n","$ $ $ $")
+for i in range(4):
+	for j in range(i+1):
+		print("$",end="")
+	print("\n")
+for i in range(16):
+	print("#",end="")
+print("\n\n")
+
+print("Jesso\n",23)
+
+for i in range(5):
+	print("&*")
+	for i in range(10):
+		print("%$")
