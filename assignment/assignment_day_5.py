247cd69977a6d47d9cdca6e6c4c2e0439e9004a2
jesso john joy <jesso1908joy@gmail.com>
2021-01-24 20:32:11 -0500

    question 3 to 10

--- assignment/assignment_day_5.py
+++ assignment/assignment_day_5.py
@@ -4,3 +4,5 @@ high_price=[4458.95,1458.3,14670.5,14765.45,14625.9]
 close_prices=[14287.5,14570,14645.15,14598.6,14380.15]
 volume=[13.2,11,8.85,9.92,16.4]
 print("Open price:",open_prices,"\nlow prices:",low_prices,"\nHigh prices:",high_price,"\nClose prices:",close_prices,"\nVolume :",volume)
+total_volume=sum(volume)
+print("total_volume",total_volume,"million")
