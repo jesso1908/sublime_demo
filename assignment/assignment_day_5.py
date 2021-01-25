e497ce9b3e1a91416bf7c48c3884dd1d0b1db49a
jesso john joy <jesso1908joy@gmail.com>
2021-01-24 20:06:35 -0500

    assgnment day 8

---
+++ assignment/assignment_day_5.py
@@ -0,0 +1,6 @@
+open_prices=[14458.5,14380.5,14581,14711.05,14593]
+low_prices=[14250,14364.25,14524.1,14508.3,14356]
+high_price=[4458.95,1458.3,14670.5,14765.45,14625.9]
+close_prices=[14287.5,14570,14645.15,14598.6,14380.15]
+volume=[13.2,11,8.85,9.92,16.4]
+print("Open price:",open_prices,"\nlow prices:",low_prices,"\nHigh prices:",high_price,"\nClose prices:",close_prices,"\nVolume :",volume)
