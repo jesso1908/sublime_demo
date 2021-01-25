602df4e646d2d5e41b3f9e4fb80159d842910eef
jesso john joy <jesso1908joy@gmail.com>
2021-01-24 20:15:21 -0500

    second question

---
+++ assignment/secondquestion.py
@@ -0,0 +1,4 @@
+last_Week_data=[[14458.5,14380.5,14581,14711.05,14593],[14250,14364.25,14524.1,14508.3,14356],
+[4458.95,1458.3,14670.5,14765.45,14625.9],[14287.5,14570,14645.15,14598.6,14380.15],[13.2,11,8.85,9.92,16.4]]
+print("open price",last_Week_data[0],"\nlow price",last_Week_data[1],
+	"\nhigh price",last_Week_data[2],"\nclose price",last_Week_data[3],"\nvolume\t",last_Week_data[4])
